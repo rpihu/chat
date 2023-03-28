d7fbceb98e9f1a71358b9ae75cc30d64ffeed51e
rpihu <75252870+rpihu@users.noreply.github.com>
2023-03-28 23:37:59 +0200

    Add files via upload

---
+++ Listener.ps1
@@ -0,0 +1,27 @@
+﻿$multicastAddress = '239.0.0.1'
+$port = 123
+
+$udpClient = New-Object System.Net.Sockets.UdpClient($port)
+$multicastEndpoint = New-Object System.Net.IPEndPoint([System.Net.IPAddress]::Parse($multicastAddress), $port)
+$udpClient.JoinMulticastGroup($multicastEndpoint.Address)
+$udpClient.Client.ReceiveTimeout = 1000
+
+Write-Host "Press Ctrl+C to stop the listener"
+
+try {
+    while ($true) {
+        $sender = New-Object System.Net.IPEndPoint([System.Net.IPAddress]::Any, 0)
+        try {
+            $bytes = $udpClient.Receive([ref]$sender)
+            $message = [System.Text.Encoding]::ASCII.GetString($bytes)
+            Write-Host "[$($sender.Address)] $message"
+        } catch [System.Net.Sockets.SocketException] {
+            if ($_.Exception.ErrorCode -ne 10060) { # WSAETIMEDOUT
+                throw
+            }
+        }
+    }
+} catch {
+    $udpClient.Close()
+    Write-Host "Listener stopped"
+}
---
+++ Sender.ps1
@@ -0,0 +1,16 @@
+﻿$multicastAddress = '239.0.0.1'
+$port = 123
+
+$udpClient = New-Object System.Net.Sockets.UdpClient
+$multicastEndpoint = New-Object System.Net.IPEndPoint([System.Net.IPAddress]::Parse($multicastAddress), $port)
+
+while ($true) {
+    Write-Host "Enter your message: " -NoNewline
+    $messageToSend = Read-Host
+
+    if ($messageToSend -ne "") {
+        $bytes = [System.Text.Encoding]::ASCII.GetBytes($messageToSend)
+        $udpClient.Send($bytes, $bytes.Length, $multicastEndpoint)
+        Write-Host "You: $messageToSend"
+    }
+}
